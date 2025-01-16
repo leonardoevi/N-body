b7570312054fae4bdf81cc8d6eb727ef69b515fd
leonardoevi <leonardo.evi@gmail.com>
2025-01-16 12:10:33 +0100

    Fix device synchronization. Test on moore particles.

--- src/cuda/main.cu
+++ src/cuda/main.cu
@@ -6,8 +6,6 @@
 #include "kernels/kernels.h"
 #include "futils/futils.h"
 
-#define COMPONENT 1     // x = 0, y = 1, z = 2. Must be < DIM
-
 int main() {
     std::cout << N_PARTICLES << " particles." << std::endl;
 
@@ -26,13 +24,11 @@ int main() {
     cudaMalloc(&device_pos, N_PARTICLES * DIM * sizeof(double));        checkCudaError("cudaMalloc2");
     cudaMalloc(&device_vel, N_PARTICLES * DIM * sizeof(double));        checkCudaError("cudaMalloc2.5");
 
-    // fill position and velocity array
-    std::vector<double> tmp{1.0, 3.0, 1.0, 3.0, 1.0, 1.0, 3.0, 3.0};
-    for (int i = 0; i < N_PARTICLES * DIM; i++) {
-        pos[i] = tmp[i];
-        vel[i] = 0.0;
-    }
+    // fill initial conditions
+    fill_array(pos, N_PARTICLES*DIM);
+    fill_array(vel, N_PARTICLES*DIM);
 
+    // copy initial condition on deviuce
     cudaMemcpy(device_pos, pos, N_PARTICLES * DIM * sizeof(double), cudaMemcpyHostToDevice); checkCudaError("cudaMalloc4");
     cudaMemcpy(device_vel, vel, N_PARTICLES * DIM * sizeof(double), cudaMemcpyHostToDevice); checkCudaError("cudaMalloc4.5");
 
@@ -55,39 +51,21 @@ int main() {
         cudaDeviceSynchronize();
     }
 
-    // allocate force array on host for testing
-    auto *force = static_cast<double *>(malloc(N_PARTICLES * DIM * sizeof(double)));
-    cudaMemcpy(force, device_force, N_PARTICLES * DIM * sizeof(double), cudaMemcpyDeviceToHost);
-    checkCudaError("copy force back");
-
     // apply force on particles
     apply_motion<<<N_PARTICLES % 1024 == 0 ? N_PARTICLES / 1024 : N_PARTICLES / 1024 + 1,1024>>>(device_pos, device_vel, device_force, N_PARTICLES, forwardEuler, D_T);
+    cudaDeviceSynchronize();
 
     // copy back new position
     cudaMemcpy(pos, device_pos, N_PARTICLES * DIM * sizeof(double), cudaMemcpyDeviceToHost); checkCudaError("cudaMalloc9");
     cudaMemcpy(vel, device_vel, N_PARTICLES * DIM * sizeof(double), cudaMemcpyDeviceToHost); checkCudaError("cudaMalloc9.5");
 
 
-    // check correctness of result
-    //const double error = check_array_force_component(f_tot, pos, COMPONENT, N_PARTICLES);
-    //std::cout << "Total result error is: " << error << std::endl;
-
-    printArray("force x", force, N_PARTICLES);
-    printArray("force y", force + N_PARTICLES, N_PARTICLES);
-
-    printArray("pos x", pos, N_PARTICLES);
-    printArray("pos y", pos + N_PARTICLES, N_PARTICLES);
-
-    printArray("vel x", vel, N_PARTICLES);
-    printArray("vel y", vel + N_PARTICLES, N_PARTICLES);
-
     // free space on Host and device
     cudaFree(device_matrix);
 
-    free(pos);
-    cudaFree(device_pos);
+    free(pos);  cudaFree(device_pos);
+    free(vel);  cudaFree(device_vel);
 
-    free(force);
     cudaFree(device_force);
 
     return EXIT_SUCCESS;
