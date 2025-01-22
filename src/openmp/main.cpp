05db3c77ca40b6d2fe34f9b57db691fc1df8254c
alesiodemiri <alesio.demiri@mail.polimi.it>
2025-01-22 18:47:21 +0100

    Add initial positions and velocities as user's input option

--- src/openmp/main.cpp
+++ src/openmp/main.cpp
@@ -7,9 +7,7 @@
 #include "Solver/LeapFrogSolver.h"
 #include "../../include/define.h"
 
-void initialize_particles(std::vector<Vector>& positions, std::vector<Vector>& velocities, int numParticles, double radius, int layers) {
-    const double gravitationalConstant = 9.81;
-
+void initialize_particles(std::vector<Vector>& positions, std::vector<Vector>& velocities, const int numParticles, const double radius, const int layers) {
     // Total number of particles per layer
     int particlesPerLayer = numParticles / layers;
 
@@ -21,11 +19,12 @@ void initialize_particles(std::vector<Vector>& positions, std::vector<Vector>& v
         double currentRadius = radius * (layer + 1) / layers;
 
         for (int j = 0; j < particlesPerLayer; ++j) {
+            constexpr double gravitationalConstant = 9.81;
             // Particle index in the overall array
             int index = layer * particlesPerLayer + j;
 
             // Random azimuthal angle theta in the range [0, 2π)
-            double theta = (j / (double)particlesPerLayer) * 2 * M_PI;
+            const double theta = (j / static_cast<double>(particlesPerLayer)) * 2 * M_PI;
 
             // Position on the circle/sphere
             if (DIM == 3) {
@@ -58,35 +57,121 @@ void initialize_particles(std::vector<Vector>& positions, std::vector<Vector>& v
     }
 }
 
-int main() {
+void print_usage(const char* executable_name) {
+    std::cout << "Usage with specific initial positions and velocities:" << std::endl;
+    std::cout << "\t" << std::filesystem::path(executable_name).filename().string() << " delta_time total_time {FE | LF} input.txt()" << std::endl;
 
-    int dimension = DIM;
-    int num_particles = NUM_PARTICLES;
+    std::cout << "Usage with generated initial positions and velocities:" << std::endl;
+    std::cout << "\t" << std::filesystem::path(executable_name).filename().string() << " delta_time total_time {FE | LF}" << std::endl;
+}
 
-    constexpr double total_time = 20.0;
-    constexpr double delta = 0.001;
-    std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+int main(int argc, char* argv[]) {
 
-    // Initializing positions and velocities randomly
-    std::vector<Vector> positions(num_particles, Vector(dimension));
-    std::vector<Vector> velocities(num_particles, Vector(dimension));
-    std::vector<double> masses(num_particles, 0.1);
-
-    const double radius = 10.0;
-    const int layers = 2;
-    initialize_particles(positions, velocities, num_particles, radius, layers);
-
-    //Initializing masses
-    for (int i = 0; i < num_particles; i++) {
-        masses[i] = 1.0;
+    if (argc != 4 && argc != 5) {
+        print_usage(argv[0]);
+        return 1;
     }
 
-    std::unique_ptr<Solver> forwardEulerSolver = std::make_unique<ForwardEulerSolver>(dimension, num_particles, total_time, delta, masses, positions, velocities);
-    std::unique_ptr<Solver> leapFrogSolver = std::make_unique<LeapFrogSolver>(dimension, num_particles, total_time, delta, masses, positions, velocities);
+    try {
+        double delta_time = std::stod(argv[1]);
+        double total_time = std::stod(argv[2]);
 
-    forwardEulerSolver->simulate("output_forward_euler.txt");
-    leapFrogSolver->simulate("output_leapfrog.txt");
+        std::string integration_name = argv[3];
 
+        if (integration_name.size() != 2) {
+            std::cout << "Integration type not supported." << std::endl;
+            std::cout << "Integration supported: FE or BE" << std::endl;
+            return 1;
+        }
+
+        if (argc == 4) {
+            int dimension = DIM;
+            int num_particles = NUM_PARTICLES;
+
+            std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+            // Initializing positions and velocities randomly
+            std::vector<Vector> positions(num_particles, Vector(dimension));
+            std::vector<Vector> velocities(num_particles, Vector(dimension));
+            std::vector<double> masses(num_particles, 0.1);
+
+            //Generated initial positions and vectors
+            const double radius = 10.0;
+            const int layers = 2;
+            initialize_particles(positions, velocities, num_particles, radius, layers);
+
+            //Initializing masses
+            for (int i = 0; i < num_particles; i++) {
+                masses[i] = 1.0;
+            }
+            if (integration_name == "LF") {
+
+                std::unique_ptr<Solver> leapFrogSolver = std::make_unique<LeapFrogSolver>(dimension, num_particles, total_time, delta_time, masses, positions, velocities);
+                std::cout << "Leap Frog solver initial energy: " << leapFrogSolver->compute_energy()<< std::endl;
+                leapFrogSolver->simulate("output_leapfrog.txt");
+                std::cout << "Leap Frog solver final energy: " << leapFrogSolver->compute_energy()<< std::endl;
+
+            }else if (integration_name == "FE") {
+
+                std::unique_ptr<Solver> forwardEulerSolver = std::make_unique<ForwardEulerSolver>(dimension, num_particles, total_time, delta_time, masses, positions, velocities);
+                std::cout << "Forward Euler solver initial energy: " << forwardEulerSolver->compute_energy()<< std::endl;
+                forwardEulerSolver->simulate("output_forward_euler.txt");
+                std::cout << "Forward Euler solver final energy: " << forwardEulerSolver->compute_energy()<< std::endl;
+
+            }
+        } else {
+            std::string input_file = argv[4];
+            std::ifstream file(input_file);
+
+            if (!file.is_open()) {
+                std::cout << "Error opening file" << std::endl;
+                return 1;
+            }
+
+            int num_particles, dimension;
+            file >> num_particles >> dimension;
+            std::cout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+
+            std::vector<Vector> positions(num_particles, Vector(dimension));
+            std::vector<Vector> velocities(num_particles, Vector(dimension));
+            std::vector<double> masses(num_particles);
+
+            for (int i = 0; i < num_particles; i++) {
+                file >> masses[i];
+            }
+
+            for (int i = 0; i < num_particles; i++) {
+
+                for (int j = 0; j < dimension; j++) {
+                    file >> positions[i][j];
+                }
+                for (int j = 0; j < dimension; j++) {
+                    file >> velocities[i][j];
+                }
+            }
+
+            if (integration_name == "LF") {
+
+                std::unique_ptr<Solver> leapFrogSolver = std::make_unique<LeapFrogSolver>(dimension, num_particles, total_time, delta_time, masses, positions, velocities);
+                std::cout << "Leap Frog solver initial energy: " << leapFrogSolver->compute_energy()<< std::endl;
+                leapFrogSolver->simulate("output_leapfrog.txt");
+                std::cout << "Leap Frog solver final energy: " << leapFrogSolver->compute_energy()<< std::endl;
+
+            }else if (integration_name == "FE") {
+
+                std::unique_ptr<Solver> forwardEulerSolver = std::make_unique<ForwardEulerSolver>(dimension, num_particles, total_time, delta_time, masses, positions, velocities);
+                std::cout << "Forward Euler solver initial energy: " << forwardEulerSolver->compute_energy()<< std::endl;
+                forwardEulerSolver->simulate("output_forward_euler.txt");
+                std::cout << "Forward Euler solver final energy: " << forwardEulerSolver->compute_energy()<< std::endl;
+
+            }
+
+        }
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error parsing input: " << e.what() << "\n";
+        return 1;
+    }
 
     return 0;
 }
