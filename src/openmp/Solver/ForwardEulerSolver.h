4db21cc27f003499540c54174b7d464c38703c46
alesiodemiri <alesio.demiri@mail.polimi.it>
2025-01-22 17:23:57 +0100

    Refactoring remaining classes

--- src/openmp/Solver/ForwardEulerSolver.h
+++ src/openmp/Solver/ForwardEulerSolver.h
@@ -1,51 +1,21 @@
-#ifndef FORWARD_EULER_SOLVER_H
-#define FORWARD_EULER_SOLVER_H
+#ifndef FORWARDEULERSOLVER_H
+#define FORWARDEULERSOLVER_H
 
 #include "Solver.h"
 #include "../Vector.h"
 
-/**
- * Child class of Solver that implements Forward Euler integration
- * v^(k+1) = v^(k) + a(k) * delta_t
- * p^(k+1) = p^(k) + v^(k+1) * delta_t
- * @tparam number_particles total number of particles of the system
- * @tparam dimension n dimension of each vector of the system (either 2 or 3)
- */
-
-template <unsigned int number_particles, unsigned int dimension> class ForwardEulerSolver : public Solver<number_particles, dimension> {
-
+class ForwardEulerSolver : public Solver {
     public:
-    /**
-     * Constructor of ForwardEulerSolver class
-     * @param total_time amount of time to run the simulation
-     * @param delta_time time step of each iteration of the simulation
-     * @param mass array of particles' masses
-     * @param initial_positions array of particles' initial positions
-     * @param initial_velocities array of particles' initial velocities
-     */
-    ForwardEulerSolver(double total_time, double delta_time,
-             const vector<double>& mass,
-             const vector<Vector<dimension>>& initial_positions,
-             const vector<Vector<dimension>>& initial_velocities)
-          : Solver<number_particles, dimension>(total_time, delta_time,mass, initial_positions, initial_velocities) {}
+      ForwardEulerSolver(int dimension, int num_particles, double total_time, double delta_time,
+                         const std::vector<double>& masses,
+                         const std::vector<Vector>& init_positions,
+                         const std::vector<Vector>& init_velocities)
+      : Solver(dimension, num_particles, total_time, delta_time, masses, init_positions,init_velocities) {}
 
-    /**
-    * Overridden method to implement Forward Euler Integration
-    */
-    void applyMotion() override {
-            #pragma omp for
-            for (int i = 0; i < number_particles; ++i) {
-
-                Vector<dimension> force;
-                for (int j = 0; j < number_particles; ++j) {
-                    force += this->accelerations[i][j];
-                }
-
-                this->velocities[i] += force * this->delta_time;
-                this->positions[i] += this->velocities[i] * this->delta_time;
-            }
-          }
-
+      void apply_motion() override;
+      void simulate(const std::string &output_file_name) override;
+      void end_timer() override;
 };
-#endif //FORWARD_EULER_SOLVER_H
 
+
+#endif //FORWARDEULERSOLVER_H
